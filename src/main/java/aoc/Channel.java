723fe00598f80c9c039b33e6c7a018e56a1ea4be
KevPB <kevin.paratre-badois@etudiant.univ-rennes1.fr>
2018-11-07 18:09:22 +0100

    Channel

---
+++ src/main/java/aoc/Channel.java
@@ -0,0 +1,16 @@
+package aoc;
+
+import java.util.Observable;
+import java.util.concurrent.Future;
+
+public class Channel implements ObserverGenerator, ObserverGeneratorAsync {
+	
+	private Display display;
+
+	@Override
+	public void update(Generator subject) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
