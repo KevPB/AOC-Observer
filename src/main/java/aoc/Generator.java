c473abe30d997da7f0425d64a3d5cfae882b6ca2
phi <alexandre.phi.vu@gmail.com>
2018-11-07 18:09:44 +0100

    Update Generator and ObserverGeneratorAsync

--- src/main/java/aoc/Generator.java
+++ src/main/java/aoc/Generator.java
@@ -1,5 +1,30 @@
 package aoc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
 public class Generator {
+
+    private int value;
+    private List<ObserverGeneratorAsync> observerAsyncs;
+
+    public Generator(){
+        this.value = 0;
+        this.observerAsyncs = new ArrayList<>();
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public void increment(){
+        this.value++;
+        for(ObserverGeneratorAsync observer : observerAsyncs){
+            observer.update(this);
+        }
+    }
+
+
 
 }
--- src/main/java/aoc/ObserverGeneratorAsync.java
+++ src/main/java/aoc/ObserverGeneratorAsync.java
@@ -1,5 +1,10 @@
 package aoc;
 
-public interface ObserverGeneratorAsync {
+import java.util.Observer;
+import java.util.concurrent.Future;
+
+public interface ObserverGeneratorAsync extends Observer {
+
+    public Future update(Generator generator);
 
 }
